62a00d0a0cd6ff3cc2aa89ae0fc82d0f6675a398
Mina <MinaDawood>
2020-09-01 16:46:57 +0200

    First commit

---
+++ src/build/precache-manifest.b838f34ee31de1c6b678784eec01fafb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e653cb97bcb48a17462ba45f8bbddf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c0360643f487b1dbc7d",
+    "url": "/static/css/main.bbd23df2.chunk.css"
+  },
+  {
+    "revision": "4bd91d0dfe58ce6ae019",
+    "url": "/static/js/2.ca213e05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca213e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c0360643f487b1dbc7d",
+    "url": "/static/js/main.be2f0856.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
